f8cec6250d00dca9765cb6bcd5150431c91f3f52
ssussu <ssussu00623@gmail.com>
2025-03-25 12:16:45 +0900

    == main

---
+++ repository/db.js
@@ -0,0 +1,14 @@
+/**
+ * 1. DB 연동 라이브러리 호출 - DB서버주소, user, password, port
+ */
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'shoppy-redux-db.c5c00c6s66l9.ap-northeast-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'admin',
+    password: 'Hwangsu94!',
+    database: 'hrdb2019',
+});
+
+export const db = pool.promise();
